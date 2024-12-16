3ca9e8049ce755d7441da0995faa2e78107f4a27
AZa0Ra <rostyk.gydzelyak@gmail.com>
2024-12-17 01:01:57 +0200

    Adding Readme

--- api/obj/Debug/net8.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f61399421bdc9827acbe4947f3a2c147b4457fe8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f17543f336cda2edf0c4593ba978a8bd83ef7695")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
